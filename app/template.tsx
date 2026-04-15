fd3a97b6e193a99f6649d8ee530defb39c2a0626
mycasevalues <admin@mycasevalues.com>
2026-04-14 20:44:12 -0400

    Update template.tsxfix: clean template.tsx indentation

--- app/template.tsx
+++ app/template.tsx
@@ -2,9 +2,8 @@ import { type ReactNode } from 'react';
 
 export default function Template({ children }: { children: ReactNode }) {
   return (
-      <div className="animate-page-enter">
-            {children}
-                </div>
-                  );
-                  }
-                  
+    <div className="animate-page-enter">
+      {children}
+    </div>
+  );
+}
